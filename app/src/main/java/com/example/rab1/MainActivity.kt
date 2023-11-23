ee5b79b4485343d60830f552144285baf328c5a7
singto908 <96246622+singto908@users.noreply.github.com>
2023-11-23 18:11:28 +0700

    APP TOOD

---
+++ app/src/main/java/com/example/rab1/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.rab1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.rab1.ui.theme.Rab1Theme
+
+data class Person(val name: String, val imageId: Int, val studentId: String)
+
+class MainActivity : ComponentActivity() {
+
+    val persons: List<Person> = listOf<Person>(
+        Person("SINGTO", R.drawable.singto, "001"),
+        Person("BEAM", R.drawable.beam, "002"),
+        Person("JAN", R.drawable.jan, "003"),
+        Person("KI", R.drawable.ki, "004"),
+        Person("BANK", R.drawable.bank, "005"),
+
+        )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Rab1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LazyColumn {
+                        items(persons) { person ->
+                            PersonListItem(person)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PersonListItem(data: Person) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(horizontal = 10.dp, vertical = 10.dp)
+    ) {
+        Image(
+            painter = painterResource(id = data.imageId),
+            contentDescription = "Avatar image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(80.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Column {
+            Text(data.name)
+            Text(data.studentId)
+        }
+    }
+}
